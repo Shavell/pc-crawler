60a983634b6d63193eed8449d44723a32e46d2ad
Shavell <12944270+Shavell@users.noreply.github.com>
2019-04-10 16:14:05 +0300

    ComputerControllerTest.java class and ping test created

---
+++ backend/src/test/java/com/shavell/backend/api/ComputerControllerTest.java
@@ -0,0 +1,42 @@
+package com.shavell.backend.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class ComputerControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void ping() throws Exception {
+        mockMvc.perform(patch("/sendHardwareInfo"))
+                .andExpect(status().isOk());
+    }
+}
